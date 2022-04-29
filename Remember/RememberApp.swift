6aef614bb39bf5f87328aa6a13c9144e493dd5d7
Jiwon <zest1923@gmail.com>
2022-04-29 15:14:15 +0900

    chore: 프로젝트 초기 설정

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by 이지원 on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
