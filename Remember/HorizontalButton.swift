198bb235a72c7c679d3a28937a2ac0f8249bf210
Jiwon <zest1923@gmail.com>
2022-04-29 19:29:11 +0900

    feat: Button view 생성
    Rectangle을 이용한 가로 버튼, 세로 버튼 생성

---
+++ Remember/HorizontalButton.swift
@@ -0,0 +1,24 @@
+//
+//  HorizontalButton.swift
+//  Remember
+//
+//  Created by 이지원 on 2022/04/29.
+//
+
+import SwiftUI
+
+struct HorizontalButton: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.white)
+            .cornerRadius(10)
+            .frame(width: 350, height: 120)
+            .shadow(color: Color(hex: "5F84C3"), radius: 5)
+    }
+}
+
+struct HorizontalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HorizontalButton()
+    }
+}
---
+++ Remember/VerticalButton.swift
@@ -0,0 +1,24 @@
+//
+//  VerticalButton.swift
+//  Remember
+//
+//  Created by 이지원 on 2022/04/29.
+//
+
+import SwiftUI
+
+struct VerticalButton: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.white)
+            .frame(width: 150, height: 200)
+            .cornerRadius(10)
+            .shadow(color: Color(hex: "5F84C3"), radius: 5)
+    }
+}
+
+struct VerticalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        VerticalButton()
+    }
+} 
